6c93394cc7470ca3661ffec678e30d730599386b
Aman Gupta <amangupta.1990@ymail.com>
2021-03-30 17:07:21 +0530

    temp commit

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,8 @@ let canvasContainer =document.getElementById("canvas-container");
 function addVideoStream(streamId){
     let streamDiv=document.createElement("div"); // Create a new div for every stream
     streamDiv.id=streamId;                       // Assigning id to div
-    streamDiv.style.transform="rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
+    streamDiv.style.transform="rotateY(180deg)";
+    debugger; // Takes care of lateral inversion (mirror image)
     remoteContainer.appendChild(streamDiv);      // Add new div to container
 }
 /**
@@ -45,7 +46,7 @@ function addCanvas(streamId){
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
     });
-
+    debugger;
     video.addEventListener('play', function() {
         var $this = this; //cache
         (function loop() {
@@ -57,24 +58,43 @@ function addCanvas(streamId){
     }, 0);
 }
 
-// Client Setup
-// Defines a client for RTC
-let client = AgoraRTC.createClient({
+//teacherclient 
+let teacherClient = AgoraRTC.createClient({
     mode: 'live',
     codec: "h264"
 });
 
+var url_string = document.location.href; //window.location.href
+var url = new URL(url_string);
+var user = url.searchParams.get("user");
+
+
+// Client Setup
+// Defines a client for RTC
+let student = AgoraRTC.createClient({
+    mode: 'live',
+    codec: "h264"
+})
+
+
+if(user === "teacher")
+setupTeacher(teacherClient);
+else 
+setupStudent(student)
+
+function setupTeacher(client){
+
 // Client Setup
 // Defines a client for Real Time Communication
-client.init("<---app id--->",() => console.log("AgoraRTC client initialized") ,handleFail);
+client.init("10351fd4ffb4448d8e3ca8f36c638057",() => console.log("AgoraRTC client initialized") ,handleFail);
 
 // The client joins the channel
-client.join(null,"any-channel",null, (uid)=>{
+client.join(null,"classroom",null, (uid)=>{
 
     // Stream object associated with your web cam is initialized
     let localStream = AgoraRTC.createStream({
         streamID: uid,
-        audio: false,
+        audio: true,
         video: true,
         screen: false
     });
@@ -98,10 +118,88 @@ client.on('stream-added', function (evt) {
 //When you subscribe to a stream
 client.on('stream-subscribed', function (evt) {
     let stream = evt.stream;
-    addVideoStream(stream.getId());
-    stream.play(stream.getId());
-    addCanvas(stream.getId());
+    addVideoStream(String(stream.getId()));
+    stream.play(String(stream.getId()));
+    addCanvas(String(stream.getId()));
 });
 //When a person is removed from the stream
 client.on('stream-removed',removeVideoStream);
 client.on('peer-leave',removeVideoStream);
+}
+
+
+function setupStudent(client){
+
+    // Client Setup
+    // Defines a client for Real Time Communication
+    client.init("10351fd4ffb4448d8e3ca8f36c638057",() => console.log("AgoraRTC client initialized") ,handleFail);
+    
+    // The client joins the channel
+    client.join(null,"classroom",null, (uid)=>{
+    
+        // Stream object associated with your web cam is initialized
+        let localStream = AgoraRTC.createStream({
+            streamID: uid,
+            audio: true,
+            video: true,
+            screen: false
+        });
+    
+        // Associates the stream to the client
+        localStream.init(function() {
+    
+            //Plays the localVideo
+            localStream.play('me');
+    
+            //Publishes the stream to the channel
+            client.publish(localStream, handleFail);
+    
+        },handleFail);
+
+
+        client.join(null,"breakout",null, (uid)=>{
+    
+            // Stream object associated with your web cam is initialized
+            let localStream = AgoraRTC.createStream({
+                streamID: uid,
+                audio: true,
+                video: true,
+                screen: false
+            });
+        
+            // Associates the stream to the client
+            localStream.init(function() {
+        
+                //Plays the localVideo
+                localStream.play('me');
+        
+                //Publishes the stream to the channel
+                client.publish(localStream, handleFail);
+        
+            },handleFail);
+        
+        },handleFail);
+    
+    },handleFail);
+
+
+   
+
+
+
+    //When a stream is added to a channel
+    client.on('stream-added', function (evt) {
+        client.subscribe(evt.stream, handleFail);
+    });
+    //When you subscribe to a stream
+    client.on('stream-subscribed', function (evt) {
+        let stream = evt.stream;
+        addVideoStream(String(stream.getId()));
+        stream.play(String(stream.getId()));
+        addCanvas(String(stream.getId()));
+    });
+    //When a person is removed from the stream
+    client.on('stream-removed',removeVideoStream);
+    client.on('peer-leave',removeVideoStream);
+    }
+
